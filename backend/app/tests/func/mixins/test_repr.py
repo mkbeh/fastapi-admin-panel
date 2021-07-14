c0bbaccdaeaf9f6fd30e2acd31afadd114d6cc38
mkbeh <gsilents226@gmail.com>
2021-07-14 15:37:07 +0300

    fix mixins tests

--- backend/app/tests/func/mixins/test_repr.py
+++ backend/app/tests/func/mixins/test_repr.py
@@ -1,16 +1,60 @@
 import pytest
+import sqlalchemy as sa
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
 
-from tests.utils import get_account_data
-from models import Account
+from db.mixins import ReprMixin
+from core.settings import settings
+from tests.utils import faker
+from tests.func.mixins.utils import in_tx
 
 
-accounts_data = [get_account_data(exclude=['password2']) for _ in range(3)]
+Base = declarative_base()
+engine = create_async_engine(settings.SQLALCHEMY_DATABASE_URI)
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+
+class BaseModel(Base, ReprMixin):
+    """Model to use as base."""
+
+    __abstract__ = True
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String)
+
+
+class User(BaseModel):
+    """User model exemple."""
+
+    __tablename__ = 'test_user'
+    __repr_attrs__ = ['name']
+
+    __mapper_args__ = {"eager_defaults": True}
+
+
+@pytest.fixture(scope="module", autouse=True)
+async def setup_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(BaseModel.metadata.create_all)
+
+    yield
+
+    async with engine.begin() as conn:
+        await conn.run_sync(BaseModel.metadata.drop_all)
+
+
+users_data = [{'name': faker.name()} for _ in range(3)]
 
 
 @pytest.mark.asyncio
-@pytest.mark.parametrize('data', accounts_data)
-async def test_repr(db, data):
-    account = await Account.create(db, **data)
-    for attr in Account.__repr_attrs__:
-        val = getattr(account, attr)
-        assert val[:Account.__repr_max_length__] in repr(account)
+@pytest.mark.parametrize('data', users_data)
+async def test_repr(data):
+    async with in_tx(async_session) as db:
+        user = User(**data)
+        db.add(user)
+        await db.commit()
+
+        for attr in User.__repr_attrs__:
+            val = getattr(user, attr)
+            assert val[:User.__repr_max_length__] in repr(user)
--- backend/app/tests/func/mixins/test_timestamp.py
+++ backend/app/tests/func/mixins/test_timestamp.py
@@ -2,11 +2,52 @@ import time
 from datetime import datetime
 
 import pytest
-from sqlalchemy import select
+import sqlalchemy as sa
+from sqlalchemy import select, insert
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
 
-from models import Account
 from db.mixins import TimestampsMixin
+from core.settings import settings
 from tests.utils import faker
+from tests.func.mixins.utils import in_tx
+
+
+Base = declarative_base()
+engine = create_async_engine(settings.SQLALCHEMY_DATABASE_URI)
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+
+class BaseModel(Base, TimestampsMixin):
+    """Model to use as base."""
+
+    __abstract__ = True
+
+    id = sa.Column(sa.Integer, primary_key=True)
+    name = sa.Column(sa.String)
+
+
+class User(BaseModel):
+    """User model exemple."""
+
+    __tablename__ = 'test_user'
+
+    __mapper_args__ = {"eager_defaults": True}
+
+
+@pytest.fixture(scope="class", autouse=True)
+async def setup_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(BaseModel.metadata.create_all)
+        await conn.execute(
+            insert(User).values(dict(name='Jonh'))
+        )
+
+    yield
+
+    async with engine.begin() as conn:
+        await conn.run_sync(BaseModel.metadata.drop_all)
 
 
 @pytest.mark.incremental
@@ -19,33 +60,37 @@ class TestTimestamps:
         ), "TimestampsMixin must have attribute __abstract__"
         assert TimestampsMixin.__abstract__ is True, "__abstract__ must be True"
 
-    async def test_timestamp_has_datetime_columns(self, db):
+    async def test_timestamp_has_datetime_columns(self):
         """Test whether TimestampsMixin has attrs created_at and updated_at."""
-        account = await select(Account).first(db)
+        async with in_tx(async_session) as tx:
+            user = await tx.scalar(select(User))
 
-        assert hasattr(
-            Account, "created_at"
-        ), "Timestamp doesn't have created_at attribute."
-        assert isinstance(
-            account.created_at, datetime
-        ), "created_at column should be datetime"
+            assert hasattr(
+                User, "created_at"
+            ), "Timestamp doesn't have created_at attribute."
+            assert isinstance(
+                user.created_at, datetime
+            ), "created_at column should be datetime"
 
-        assert hasattr(
-            Account, "updated_at"
-        ), "Timestamp doesn't have updated_at attribute."
-        assert isinstance(
-            account.updated_at, datetime
-        ), "updated_at column should be datetime"
+            assert hasattr(
+                User, "updated_at"
+            ), "Timestamp doesn't have updated_at attribute."
+            assert isinstance(
+                user.updated_at, datetime
+            ), "updated_at column should be datetime"
 
-    async def test_updated_at_column_must_change_value(self, db):
+    async def test_updated_at_column_must_change_value(self):
         """Test whether updated_at value is most recently after update."""
-        await db.commit()
+        async with in_tx(async_session) as tx:
+            user = await tx.scalar(select(User))
 
-        account = await select(Account).first(db)
-        dt_1 = account.updated_at
-        time.sleep(0.3)
-        await account.update(db, fullname=faker.name())
+            dt_1 = user.updated_at
+            time.sleep(0.3)
 
-        dt_2 = account.updated_at
+            user.name = faker.name()
+            tx.add(user)
+            await tx.commit()
+
+            dt_2 = user.updated_at
 
         assert dt_1 < dt_2, "dt_1 should be older than dt_2"
