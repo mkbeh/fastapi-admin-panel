dc75402cd276b8d3af9cf3be95dc448e5f557df1
mkbeh <gsilents226@gmail.com>
2021-11-18 16:51:44 +0300

    feat(signals): add session signal on startup

--- backend/app/db/__init__.py
+++ backend/app/db/__init__.py
@@ -1,7 +1,3 @@
+from .init_data import create_initial_roles, create_initial_superuser
 from .init_db import db_init
-from .init_data import (
-    create_initial_superuser,
-    create_initial_roles,
-)
-
 from .orm.patcher import patch_sqlalchemy_crud
